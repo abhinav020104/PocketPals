40d43b247a222fa93c04f23ebed52fea2560f336
Abhinav Arora <abhinavarora310@gmail.com>
2024-02-16 00:09:51 +0530

    1

--- Server/Handlers/loginHandler.js
+++ Server/Handlers/loginHandler.js
@@ -7,27 +7,27 @@ const loginHandler = async(req , res)=>{
         const {UserName , Password} = req.body;
         const userDetails = await User.findOne({UserName:UserName}).populate("AccountDetails").exec();
         const JWT_SECRET = process.env.JWT_SECRET;
-    if(!userDetails){
-        return res.status(404).json({
-            success:false,
-            message:"User doesnot exist !",
-        })
-    }
-    if(await bcrypt.compare(Password , userDetails.Password)){
-        const token =  jwt.sign({UserName:UserName , Password:Password , id:userDetails._id} , JWT_SECRET);
-        userDetails.token =  token;
-        return res.status(200).json({
-            success:true,
-            message:"Login sucessfull !",
-            data:userDetails,
-        })
-    }
-    else{
-        return res.status(401).json({
-            success:false,
-            message:"Invalid password",
-        })
-    }
+        if(!userDetails){
+            return res.status(404).json({
+                success:false,
+                message:"User doesnot exist !",
+            })
+        }
+        if(await bcrypt.compare(Password , userDetails.Password)){
+            const token =  jwt.sign({UserName:UserName , Password:Password , id:userDetails._id} , JWT_SECRET);
+            userDetails.token =  token;
+            return res.status(200).json({
+                success:true,
+                message:"Login sucessfull !",
+                data:userDetails,
+            })
+        }
+        else{
+            return res.status(401).json({
+                success:false,
+                message:"Invalid password",
+            })
+        }
     }catch(error){
         console.log(error);
         return res.status(500).json({
